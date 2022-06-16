6d69bcbf22b28fb660a8dd76f3e484ad5257bdd7
Abdolvahed Safizadeh <abdolvahedsafizadeh@Abdolvaheds-iMac.local>
2022-06-16 20:18:32 +0100

    add login and register form functionality

--- pages/login.vue
+++ pages/login.vue
@@ -1,13 +1,34 @@
 <template>
-    <v-container>
-      <v-row>
-          <v-col cols="6" class="mx-auto">
-              <v-card color="transparent" title="Login">
-                  <v-btn color="primary" @click.prevent="login">Login</v-btn>
-              </v-card>
-          </v-col>
-      </v-row>
-  </v-container>
+  <div id="app">
+    <v-app id="inspire">
+        <v-container>
+          <v-row>
+              <v-col cols="6" class="mx-auto">
+                  <v-card flat color="transparent" title="Login">
+                      <v-form ref="loginForm">
+                        <v-text-field
+                        v-model="email"
+                        :rules="emailRules"
+                        label="Email"
+                        >
+                        </v-text-field>
+                        <v-text-field
+                        v-model="password"
+                        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                        :rules="passwordRules"
+                        label="Password"
+                        :type="show1 ? 'text' : 'password'"
+                        @click:append="show1 = !show1"
+                        >
+                        </v-text-field>
+                        <v-btn color="primary" @click="login">Login</v-btn>
+                      </v-form>
+                  </v-card>
+              </v-col>
+          </v-row>
+      </v-container>
+    </v-app>
+  </div>
 </template>
 
 <script>
@@ -15,19 +36,30 @@
 export default {
   data() {
     return {
-      name: 'login',
-      valid: false,
+      show1: false,
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+      password: '',
+      passwordRules: [
+        (value) => !!value || 'Please type password.',
+        (value) => (value && value.length >= 6) || 'minimum 6 characters',
+      ],
       form: {
-          email: 'vahed@yahoo.com',
-          password: 'sara123'
+          email: '',
+          password: ''
       }
     }
   },
 
   methods: {
       login() {
-          this.$axios.post('http://127.0.0.1:8000/api/login', {
-              data: this.form
+        console.log(this.email)
+          this.$axios.post('/login', {
+              email: this.email,
+              password: this.password
           })
           .then(function (response) {
               if(response.data === 1) {
--- pages/register.vue
+++ pages/register.vue
@@ -1,21 +1,84 @@
 <template>
-    <v-container>
-      <v-row>
-          <v-col cols="6" class="mx-auto">
-              <v-card flat color="transparent" title="Register">
-                  Register
-              </v-card>
-          </v-col>
-      </v-row>
-  </v-container>
+  <div id="app">
+    <v-app id="inspire">
+        <v-container>
+          <v-row>
+              <v-col cols="6" class="mx-auto">
+                  <v-card flat color="transparent" title="Register">
+                      <v-form ref="registerForm">
+                        <v-text-field
+                        v-model="name"
+                        :rules="nameRules"
+                        label="Fullname"
+                        >
+                        </v-text-field>
+                        <v-text-field
+                        v-model="email"
+                        :rules="emailRules"
+                        label="Email"
+                        >
+                        </v-text-field>
+                        <v-text-field
+                        v-model="password"
+                        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                        :rules="passwordRules"
+                        label="Password"
+                        :type="show1 ? 'text' : 'password'"
+                        @click:append="show1 = !show1"
+                        >
+                        </v-text-field>
+                        <v-btn color="primary" @click="register">Rgister</v-btn>
+                      </v-form>
+                  </v-card>
+              </v-col>
+          </v-row>
+      </v-container>
+    </v-app>
+  </div>
 </template>
-
 <script>
 export default {
-  data() {
-    return {
-      name: 'register'
+  data: () => ({
+      show1: false,
+      name: '',
+      nameRules: [ v => !!v || 'Fullname is required' ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+      password: '',
+      passwordRules: [
+        (value) => !!value || 'Please type password.',
+        (value) => (value && value.length >= 6) || 'minimum 6 characters',
+      ],
+      form: {
+          name: '',
+          email: '',
+          password: ''
+      }
+  }),
+
+  methods: {
+    register() {
+        this.$axios.post('register', {
+            name: this.name,
+            email: this.email,
+            password: this.password
+        })
+        .then(function (response) {
+            console.log(response.data)
+              if(response.data) {
+                console.log('redirect...')
+                window.location = "/login"
+              }
+          })
+          .catch(function (error) {
+              if(error) {
+                alert("Register error")
+              }
+          });  
     }
-  }
+  },
 }
 </script>
